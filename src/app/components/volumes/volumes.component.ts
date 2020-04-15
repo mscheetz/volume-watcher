e7bf979412dadc3cc6f9f2324c46bb753ca3204f
Matt Scheetz <mfscheetz@gmail.com>
2020-04-14 21:32:23 -0400

    new component

---
+++ src/app/components/volumes/volumes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volumes',
+  templateUrl: './volumes.component.html',
+  styleUrls: ['./volumes.component.css']
+})
+export class VolumesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
